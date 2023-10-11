0bdc89c4f1e874737703b37ee26a7285b74a62cb
thredghost <senaji69@gmail.com>
2023-10-11 02:55:40 -0700

    entry

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
+#include "main.h"
+
 
 /**
- * main - Pints the lowercase alphabet in reverse.
- *
- * Return: Always 0.
- */
-int main(void)
+* print_last_digit - Prints the last digit of a number.
+* @n: The number in question.
+*
+* Return: Value of the last digit.
+*/
+
+int print_last_digit(int n)
 {
-	char letter;
+	int last_digit = n % 10;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
+	if (last_digit < 0)
+		last_digit *= -1;
 
-	putchar('\n');
+	_putchar(last_digit + '0');
 
-	return (0);
+	return (last_digit);
 }
 
