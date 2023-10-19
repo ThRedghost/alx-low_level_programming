65ec62d82a72ea0ed77728ed038bed7a1acae82e
thredghost <senaji69@gmail.com>
2023-10-19 10:55:22 -0700

    entry 1

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *a);
+char *rot13(char *a);
+char *leet(char *a);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif
+
