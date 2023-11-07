db81f3fb0f7294b94c32ab1e63345dec39b6cd3c
thredghost <senaji69@gmail.com>
2023-11-06 18:01:35 -0800

    entry

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define dog_t struct dog
+#define struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
 
 /**
  * struct dog - struct for dog
